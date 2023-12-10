ad28bc0c85499485f20e7c6cf1133f0de990b25f
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-12-09 21:49:43 -0500

    Updated: Sat Dec  9 21:49:43 EST 2023

--- app/components/canvas/MainCanvas.tsx
+++ app/components/canvas/MainCanvas.tsx
@@ -1,121 +1,18 @@
 import React, { useState } from "react";
 import UncontrolledDiagram from "./diagram/UncontrolledDiagram"; // import your diagram component
-import Tree from "react-d3-tree";
+
 
 interface MainCanvasProps {
   metadata: any;
 }
 
 const MainCanvas: React.FC<MainCanvasProps> = ({ metadata }) => {
-  const orgChart = {
-    name: "CEO",
-    children: [
-      {
-        name: "Manager",
-        attributes: {
-          department: "Production",
-        },
-        children: [
-          {
-            name: "Foreman",
-            attributes: {
-              department: "Fabrication",
-            },
-            children: [
-              {
-                name: "Worker",
-              },
-            ],
-          },
-          {
-            name: "Foreman",
-            attributes: {
-              department: "Assembly",
-            },
-            children: [
-              {
-                name: "Worker",
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
-
-  const tree_data = {
-    data: orgChart,
-    totalNodeCount: 0,
-    orientation: "horizontal",
-    dimensions: undefined,
-    centeringTransitionDuration: 800,
-    translateX: 200,
-    translateY: 300,
-    collapsible: true,
-    shouldCollapseNeighborNodes: false,
-    initialDepth: 1,
-    depthFactor: undefined,
-    zoomable: true,
-    draggable: true,
-    zoom: 1,
-    scaleExtent: { min: 0.1, max: 1 },
-    separation: { siblings: 2, nonSiblings: 2 },
-    nodeSize: { x: 200, y: 200 },
-    enableLegacyTransitions: false,
-    transitionDuration: 500,
-    styles: {
-      nodes: {
-        node: {
-          circle: {
-            fill: "#52e2c5",
-          },
-          attributes: {
-            stroke: "#000",
-          },
-        },
-        leafNode: {
-          circle: {
-            fill: "transparent",
-          },
-          attributes: {
-            stroke: "#000",
-          },
-        },
-      },
-    },
-  };
+  
 
   return (
     <div className="flex-1 items-center justify-center">
-      {/* <UncontrolledDiagram metadata={metadata} /> */}
-      <div id="treeWrapper" style={{ width: "100%", height: "100%" }}>
-        <Tree
-          hasInteractiveNodes
-          data={tree_data.data}
-          rootNodeClassName="demo-node"
-          branchNodeClassName="demo-node"
-          orientation={tree_data.orientation}
-          dimensions={tree_data.dimensions}
-          centeringTransitionDuration={tree_data.centeringTransitionDuration}
-          translate={{
-            x: tree_data.translateX,
-            y: tree_data.translateY,
-          }}
-          collapsible={tree_data.collapsible}
-          initialDepth={tree_data.initialDepth}
-          zoomable={tree_data.zoomable}
-          draggable={tree_data.draggable}
-          zoom={tree_data.zoom}
-          scaleExtent={tree_data.scaleExtent}
-          nodeSize={tree_data.nodeSize}
-          separation={tree_data.separation}
-          enableLegacyTransitions={tree_data.enableLegacyTransitions}
-          transitionDuration={tree_data.transitionDuration}
-          depthFactor={tree_data.depthFactor}
-          styles={tree_data.styles}
-          shouldCollapseNeighborNodes={tree_data.shouldCollapseNeighborNodes}
-        />
-      </div>
+      <UncontrolledDiagram metadata={metadata} />
+      
     </div>
   );
 };
--- app/components/canvas/diagram/UncontrolledDiagram.tsx
+++ app/components/canvas/diagram/UncontrolledDiagram.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Diagram, { useSchema, createSchema } from "beautiful-react-diagrams";
 import { useEffect, useMemo, useState } from "react";
+import Tree from "react-d3-tree";
 
 interface UncontrolledDiagramProps {
   metadata: any;
@@ -105,9 +106,111 @@ const UncontrolledDiagram: React.FC<UncontrolledDiagramProps> = ({
     setKey((prev) => prev + 1);
   }, [metadata]); // Dependency array includes metadata
 
+  const orgChart = {
+    name: "CEO",
+    children: [
+      {
+        name: "Manager",
+        attributes: {
+          department: "Production",
+        },
+        children: [
+          {
+            name: "Foreman",
+            attributes: {
+              department: "Fabrication",
+            },
+            children: [
+              {
+                name: "Worker",
+              },
+            ],
+          },
+          {
+            name: "Foreman",
+            attributes: {
+              department: "Assembly",
+            },
+            children: [
+              {
+                name: "Worker",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  const tree_data = {
+    data: orgChart,
+    totalNodeCount: 0,
+    orientation: "horizontal",
+    dimensions: undefined,
+    centeringTransitionDuration: 800,
+    translateX: 200,
+    translateY: 300,
+    collapsible: true,
+    shouldCollapseNeighborNodes: false,
+    initialDepth: 1,
+    depthFactor: undefined,
+    zoomable: true,
+    draggable: true,
+    zoom: 1,
+    scaleExtent: { min: 0.1, max: 1 },
+    separation: { siblings: 2, nonSiblings: 2 },
+    nodeSize: { x: 200, y: 200 },
+    enableLegacyTransitions: false,
+    transitionDuration: 500,
+    styles: {
+      nodes: {
+        node: {
+          circle: {
+            fill: "#52e2c5",
+          },
+          attributes: {
+            stroke: "#000",
+          },
+        },
+        leafNode: {
+          circle: {
+            fill: "transparent",
+          },
+          attributes: {
+            stroke: "#000",
+          },
+        },
+      },
+    },
+  };
   return (
-    <div style={{ height: "100%", width: "100%" }} key={key}>
-      <Diagram schema={schema} />
+    <div id="treeWrapper" style={{ width: "100%", height: "100%" }}>
+      <Tree
+        hasInteractiveNodes
+        data={tree_data.data}
+        rootNodeClassName="demo-node"
+        branchNodeClassName="demo-node"
+        orientation={tree_data.orientation}
+        dimensions={tree_data.dimensions}
+        centeringTransitionDuration={tree_data.centeringTransitionDuration}
+        translate={{
+          x: tree_data.translateX,
+          y: tree_data.translateY,
+        }}
+        collapsible={tree_data.collapsible}
+        initialDepth={tree_data.initialDepth}
+        zoomable={tree_data.zoomable}
+        draggable={tree_data.draggable}
+        zoom={tree_data.zoom}
+        scaleExtent={tree_data.scaleExtent}
+        nodeSize={tree_data.nodeSize}
+        separation={tree_data.separation}
+        enableLegacyTransitions={tree_data.enableLegacyTransitions}
+        transitionDuration={tree_data.transitionDuration}
+        depthFactor={tree_data.depthFactor}
+        styles={tree_data.styles}
+        shouldCollapseNeighborNodes={tree_data.shouldCollapseNeighborNodes}
+      />
     </div>
   );
 };
